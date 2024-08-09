7efc15c62d3c371e0056af94899152f87e0b7a86
Israel Capetillo Sánchez <israel.capetillo@gmail.com>
2024-08-08 18:48:48 -0600

    First commit
    First commit

---
+++ 01-SayingHello/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_SayingHello;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What is your name?");
+        Console.WriteLine("Hello, " + Console.ReadLine() + " nice to meet you!!");
+    }
+}
